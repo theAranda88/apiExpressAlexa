1af921d9f9b02eea83780c67abfedcfcdcc4cf9d
Daniel Aranda <thearanda88@gmail.com>
2025-02-19 20:38:48 -0500

    mi primer commit

---
+++ app/controllers/userController.js
@@ -0,0 +1,44 @@
+let UserService = require("../services/usersService");
+
+class UserController{
+    static async obtenerUsuarios(req, res) {
+        try {
+            const users = await UserService.obtenerUser();
+            res.json(users);
+        } catch (e) {
+            res.json({ error: "error en la peticion" });
+            
+    }
+    }
+    static async login(req, res) {
+        try {
+        
+        } catch (e) {
+            
+    }
+    }
+       static async register(req, res) {
+           try {
+            let {nombre,correo,contrasena}=req.body
+               let user = await UserService.crearUsarios(nombre, correo, contrasena);
+               res.json(user);
+        } catch (e) {
+            res.json({ error: "error en la peticion" }); 
+        }
+    }
+       static async eliminarUsuarios(req, res) {
+        try {
+        
+        } catch (e) {
+            
+    }
+    }
+         static async actualizarUsuarios(req, res) {
+        try {
+        
+        } catch (e) {
+            
+    }
+    }
+}
+module.exports = UserController;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const usersRouter=require('./app/routers/usersRouter')
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+//ruta
+app.use("/api", usersRouter);
+
+
+//configuracion puerto
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log("servidor corriendo");
+})
