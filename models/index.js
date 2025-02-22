fee65e81653f34ccae312512a634a82efcc41fc6
Daniel Aranda <thearanda88@gmail.com>
2025-02-21 19:40:29 -0500

    arreglos al archivo config.json y index.js para su conexion

--- models/index.js
+++ models/index.js
@@ -6,15 +6,22 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const base = require(__dirname + '/../config/config.json')[env];
+const mysql2 = require('mysql2')
 const db = {};
+const config = {
+  username: "root",
+  password: "kNOgIucJkschMIIuUSyQhmQBZoZsMBaU",
+  database: "railway",
+  host: "metro.proxy.rlwy.net",
+  dialect: "mysql",
+  port: 22878,
+  dialectModule: mysql2,
+}
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
